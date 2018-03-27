a1779135dab43356fcff1d5e08c03bc5832dcfc6
linyongliang <932888148@qq.com>
2018-03-27 09:01:39 +0800

    xx

--- 14_springboot_initBinder/src/main/java/com/example/demo/GlobalController.java
+++ 14_springboot_initBinder/src/main/java/com/example/demo/GlobalController.java
@@ -1,19 +1,10 @@
 package com.example.demo;
 
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Date;
 
 @RestController
 public class GlobalController {
-    @RequestMapping("test/{date}")
-    public void test(@PathVariable Date date, HttpServletResponse response) throws IOException {
-        response.getWriter().write(date);
-    }
+    //http://blog.csdn.net/xsf1840/article/details/73556633
 
 }
--- 15_springboot_model/src/main/java/com/example/demo/ModelAttributeController.java
+++ 15_springboot_model/src/main/java/com/example/demo/ModelAttributeController.java
@@ -1,19 +1,43 @@
 package com.example.demo;
 
 
-import org.springframework.web.bind.annotation.PathVariable;
+import net.sf.json.JSONObject;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Date;
 
 @RestController
 public class ModelAttributeController {
-    @RequestMapping("test/{date}")
-    public void test(@PathVariable Date date, HttpServletResponse response) throws IOException {
-        response.getWriter().write(date);
+    @ModelAttribute("/loginname")
+    public String userModelfirst(@RequestParam("loginname") String loginname) {
+        System.out.println("userModelfirst->" + loginname);
+        return loginname;
     }
 
+    //通过@ResquestParam赋值给变量，再通过model.addAttribute（）方法实现model属性名称和属性值。
+    @ModelAttribute
+    public String userModelSecond(@RequestParam("loginname") String loginname, Model model) {
+        System.out.println("before->" + JSONObject.fromObject(model));
+        System.out.println("userModelSecond->" + loginname);
+        model.addAttribute("loginname", loginname);
+        System.out.println("after->" + JSONObject.fromObject(model));
+        return loginname;
+    }
+
+//    @RequestMapping和@ModelAttribute注释同一个方法时，此方法返回的不是一个视图名称，而是model的值,视图名称是"/loginthird",Model的属性名称value已经在注解中指定，所以，
+//    可以理解为username（key）= admin（value）
+    @RequestMapping(value = "/loginthird")
+    @ModelAttribute(value = "username")
+    public String loginthrid(@RequestParam("loginname") String loginname) {
+        System.out.println("loginthrid->");
+        return "admin";
+    }
+
+    @RequestMapping(value = "/loginfirst")
+    public String loginfirst() {
+        System.out.println("loginfirst->");
+        return "loginfirst";
+    }
 }
