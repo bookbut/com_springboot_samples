fb1f4dbe3e88891eea9ae7ca9ea309d6962819bb
linyongliang <932888148@qq.com>
2019-05-27 17:15:58 +0800

    xx

---
+++ 03_springboot_cxf/src/main/java/com/seatelgroup/fivepay/service/CommonService.java
@@ -0,0 +1,14 @@
+package com.seatelgroup.fivepay.service;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+@WebService(serviceName = "ComService" ,targetNamespace = "http://service.seatel.com/",endpointInterface = "com.seatel.service.CommonService")
+
+public interface CommonService {
+    @WebMethod
+    @WebResult(name = "String", targetNamespace = "")
+    public String sayHello(@WebParam(name = "userName") String name);
+}
---
+++ 08_springboot_swagger/src/main/java/com/seatelgroup/fivepay/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.seatelgroup.fivepay;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket customDocket(){
+        return  new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("文档标题")
+                .description("文档描述")
+                .contact(new Contact("aliang","http://www.baidu.com","932888148@qq.com"))
+                .license("内部使用")
+                .licenseUrl("http://localhost")
+                .version("1.0")
+                .build();
+//        return new ApiInfo("文档标题","文档描述","1.0","http://localhost/","aliangt","使用权限","http://www.baidu.com");
+    }
+}
